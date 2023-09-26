e080efd02d310fa1b935f5d14f07ce8899a3929c
Keerthana910234 <keerthm@iitk.ac.in>
2023-09-26 18:56:56 +0530

    uncommented code

--- Analysis.sh
+++ Analysis.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# Before running this, read step0.sh, step1.sh, step2.sh and step3.sh so that all details can be checked, especially if running the pipeline for a new experiment set or on a new system
-
 #SBATCH --account=b1042
 #SBATCH --partition=genomics
 #SBATCH --nodes=1
@@ -13,9 +11,10 @@
 #SBATCH --error=errlog
 
 
-# module purge
-# eval "$(conda shell.bash hook)"
-# conda activate Barcode_extraction
+module purge
+eval "$(conda shell.bash hook)"
+conda activate Barcode_extraction
+
 #Code to run the overall python script for the pipeline: This is the file where you modified all the inputs.
 python3 /home/keerthana/Goyal_Lab/FateMapPipeline_Ubuntu/gDNA_Barcode_Extraction/experimentIDInputRun.py
 
